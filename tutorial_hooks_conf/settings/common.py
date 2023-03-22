0770a5f1107af1d6b92293173a71483cfd749078
Felipe Montoya <felipe.montoya@edunext.co>
2023-03-22 13:11:44 -0500

    feat: adding all the github qa files

--- tutorial_hooks_conf/settings/common.py
+++ tutorial_hooks_conf/settings/common.py
@@ -8,8 +8,6 @@ def plugin_settings(settings):
     Set of plugin settings used by the Open Edx platform.
     More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
     """
-    # Plugin settings.
-
     settings.OPEN_EDX_FILTERS_CONFIG = {}
 
     settings.OPEN_EDX_FILTERS_CONFIG["org.openedx.learning.course_about.render.started.v1"] = {
@@ -18,4 +16,3 @@ def plugin_settings(settings):
             "tutorial_hooks_conf.pipeline.OnlyVisibleIfLoggedIn"
         ]
     }
-    print("this are the settings!!\n\n\n")
