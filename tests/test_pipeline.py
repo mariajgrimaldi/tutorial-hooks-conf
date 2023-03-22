27b526ad118ee50a88be74604e98afb6cef1b593
Felipe Montoya <felipe.montoya@edunext.co>
2023-03-22 16:05:44 -0500

    feat: adding unit testing

---
+++ tests/test_pipeline.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Tests for the `tutorial-hooks-conf` filters module.
+"""
+
+from django.test import TestCase, override_settings
+from unittest.mock import Mock, patch
+
+from openedx_filters.learning.filters import (
+    CourseAboutRenderStarted,
+)
+
+
+@override_settings(
+    OPEN_EDX_FILTERS_CONFIG={
+        "org.openedx.learning.course_about.render.started.v1": {
+            "fail_silently": False,
+            "pipeline": [
+                "tutorial_hooks_conf.pipeline.OnlyVisibleForEmailDomains"
+            ]
+        }
+    }
+)
+class OnlyVisibleForEmailDomainsTestCase(TestCase):
+    """
+    You can test your own code independently of the edx-platform repo
+    """
+
+    def test_redirect_denied(self):
+        """
+        Email domains not in the list will get a redirection
+        """
+        mock_req = Mock()
+        mock_req.user.email = "denied@not-allowed.com"
+        with patch('tutorial_hooks_conf.pipeline.crum.get_current_request', return_value=mock_req):
+            with self.assertRaises(CourseAboutRenderStarted.RedirectToPage):
+                CourseAboutRenderStarted.run_filter(context={}, template_name="some_template.html")
+
+    def test_let_allowed_pass(self):
+        """
+        Email domains in the list get the page rendered as usual.
+        """
+        mock_req = Mock()
+        mock_req.user.email = "welcome@allowed.com"
+        with patch('tutorial_hooks_conf.pipeline.crum.get_current_request', return_value=mock_req):
+            CourseAboutRenderStarted.run_filter(context={}, template_name="some_template.html")
+
