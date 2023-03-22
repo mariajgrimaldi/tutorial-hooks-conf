f42eae8106d4cffd2e763033466bf1179a8a90e8
Felipe Montoya <felipe.montoya@edunext.co>
2023-03-22 17:55:22 -0500

    feat: adding the send to googlesheets implementation

--- tutorial_hooks_conf/receivers.py
+++ tutorial_hooks_conf/receivers.py
@@ -9,3 +9,15 @@ def send_enrollment_data_to_webhook(enrollment, **kwargs):
     Listen for the enrollment event data and pass it to google sheets
     """
     envelope = kwargs.get("metadata")
+
+    requests.post(
+        "https://hooks.zapier.com/hooks/catch/105048/33sbbyf/",
+        {
+            "user_id": enrollment.user.id,
+            "username": enrollment.user.pii.name,
+            "user_email": enrollment.user.pii.email,
+            "event_type": envelope.event_type,
+            "timestamp": str(envelope.time),
+            "container": envelope.sourcehost,
+        },
+    )
